a885c249fb58843199b7b969ee21db688059ead2
karanmanral2007 <karanmanral061@gmail.com>
2026-03-27 10:45:13 +0800

    day1

---
+++ day 1/script.js
@@ -0,0 +1,46 @@
+// greet();
+
+
+// function greet(){
+//   console.log("hello");
+// }                               output - hello
+
+
+// Question 2. convert too arrow function
+// function multiply(a, b) {
+//   return a * b;
+// }
+
+// output
+//  let multiply = (a, b)=>{
+//   return a * b;
+// };
+
+
+// function sayhi(name = "Guest"){
+//   console.log("hii", name);
+// }
+
+// sayhi();             output - hii Guest
+
+
+// function checkage(age){
+//   if (age <18) return "too young";
+//   return"allowed";
+// }
+// console.log(checkage(12));          output - too young
+
+// function fn(){
+//   return;
+// }
+
+// console.log(fn());             output - undifiend
+
+
+// function abcd(val){
+//   val();
+// }
+
+// abcd(function(){
+//   console.log("heyhey");
+// });
