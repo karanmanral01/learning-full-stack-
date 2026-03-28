7c589991d53b7953fc25fc97d158d418de21ca9f
karan manral <karanmanral@gmail.com>
2026-03-28 23:39:12 +0800

    day 2

--- day 2/script.js
+++ day 2/script.js
@@ -1,28 +1,28 @@
-// Qus . 1   // let furits = ["apple", "banana", "Mango"];/
+ let furits = ["apple", "banana", "Mango"];/
 
 
-//  Qus . 2  // let fruits = ["apple", "banana",];
-// fruits.push("Mango");
-// fruits.unshift("Pineapple");
+  let fruits = ["apple", "banana",];
+fruits.push("Mango");
+fruits.unshift("Pineapple");
 
 
-//  Qus . 3  // let fruits = ["apple", "banana"]
-//     fruits.pop();
-//     fruits.push("Kiwi");
+  let fruits = ["apple", "banana"]
+    fruits.pop();
+    fruits.push("Kiwi");
 
 
-// Qus . 4  let number = [1, 2, 3, 4];
-//    number.pop();
+  let number = [1, 2, 3, 4];
+   number.pop();
 
 
-// Qus . 5  //  let colors = ["Green", "yellow"];
-//  colors.splice(1, 0, "red", "blue");
+  let colors = ["Green", "yellow"];
+ colors.splice(1, 0, "red", "blue");
 
 
-// Qus . 6  // let items = [1, 2, 3, 4, 5, 6,];
-// let newarr = items.slice(1, 4,);
-// console.log(newarr);
+let items = [1, 2, 3, 4, 5, 6,];
+let newarr = items.slice(1, 4,);
+console.log(newarr);
 
 
-// Qus . 7 // let names = ["tannu", "karan", "lucky", "anshu"];
-//  names.sort().reverse();
+let names = ["tannu", "karan", "lucky", "anshu"];
+ names.sort().reverse();
