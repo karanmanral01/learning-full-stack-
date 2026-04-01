66aac42187e0bb1a9ddd03a19ca2b0461fb03856
Karan manral <karanmanral061@gmail.com>
2026-04-01 13:20:45 +0800

    day-6

--- day-6/script.js
+++ day-6/script.js
@@ -87,5 +87,127 @@ setTimeout(function(){
 }, 3000);
 
 
+function runTwice(fn) {
+  fn();
+  fn();
+}
 
+runTwice(function(){
+    console.log("Hello");
+});
+
+
+let global = 0;
+function impure(a){
+    global++;
+    console.log(a + global);
+}
+
+impure(2);
+impure(2);
+
+
+function pure(a, b){
+  console.log(a + b);
+}
+
+pure(1, 2);
+pure(1, 2);
+
+
+function abcd({name , age} ){
+console.log(name , age);
+}
+
+abcd({name: "karan", age: 27});
+
+
+let obj = {
+   name: "karan",
+   fnc : function (){
+    let fnc2 = () => {
+        console.log(this);
+    };
+    fnc2();
+   },
+};
+
+obj.fnc();
+
+
+let arr = [1, 2, 3, 4, 5, 6];
+let newarr = arr.map(function(val){
+    return val * val;
+});
+
+console.log(newarr);
+
+
+let arr  = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10];
+let newarr = arr.filter(function (val){
+    return val % 2 === 0;
+});
+
+console.log(newarr);
+
+
+let salary = [1000, 2000, 3000]
+let ans = salary.reduce(function (acc, val){
+return acc + val;
+}, 0);
+
+console.log(ans);
+
+
+let names = [ "karan", "anshu", "sandeep", "pol", "dol"];
+let ans = names.some(function (val){
+ return val.length > 3;
+});
+
+console.log(ans);/
+
+
+let names = [ "karan", "anshu", "sandeep", "pol", "dol"];
+let ans = names.every(function (val){
+ return val.length > 3;
+});
+
+console.log(ans);
+
+
+let user = {
+    name: "karan ",
+    age: 23, 
+    email: "h@h.j",
+}
+
+Object.freeze(user);  
+user.age = 34;
+
+console.log(user);
+
+
+ let user = {
+    name: "karan ",
+    age: 23, 
+    email: "h@h.j",
+}
+
+Object.seal(user);
+user.name = "aman";
+user.social = "instagram";
+
+console.log(user);
+
+
+let user = { 
+    name: "karan",
+    age: 32,
+    address: {
+        city: "Bhopal",
+        pincode: 232434,
+    },
+}
+
+console.log(user.address.city);
 
